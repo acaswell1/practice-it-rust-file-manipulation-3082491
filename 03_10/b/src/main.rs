b9d2a0e402ec4ac11eec7f2ce2bb315e577d531e
acaswell1 <72052992+acaswell1@users.noreply.github.com>
2023-11-08 16:22:48 +0000

    finished section 3!

--- 03_10/b/src/main.rs
+++ 03_10/b/src/main.rs
@@ -1,8 +1,24 @@
+use std::fs::read_to_string;
+
 fn main() {
     let file_path = "file_with_lines";
 
     // Implement the read_file function
-    //let lines = read_file(&file_path).expect(&format!("Unable to read file <{}>", &file_path));
+    let lines = read_file(file_path).expect(&format!("Unable to read file <{}>", &file_path));
 
-    //println!("{:?}", lines);
+    println!("{:?}", lines);
+}
+
+fn read_file(file_path: &str) -> Result<Vec<Vec<String>>, std::io::Error> {
+    Ok( 
+        read_to_string(file_path)
+            .expect("Unable to read file")
+            .split('\n')
+            .map(|line| line
+                .to_string()
+                .split_whitespace()
+                .map(|word| word.to_string())
+                .collect::<Vec<String>>())
+            .collect::<Vec<Vec<String>>>()
+    )
 }
